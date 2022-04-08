fa54be9e3ac574db87025bf126911254624922d0
Felype-Henrique <felypehenrique25@gmail.com>
2022-04-08 10:20:36 -0300

    model loader

--- server/src/app/models/User.js
+++ server/src/app/models/User.js
@@ -7,7 +7,7 @@ class User extends Model {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
-        PROVIDER: Sequelize.BOOLEAN,
+        provider: Sequelize.BOOLEAN,
       },
       {
         sequelize,
